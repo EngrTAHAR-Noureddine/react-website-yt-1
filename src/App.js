36b858e448eaf592592b2a547821245bed359bc8
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-08 19:04:47 +0100

    add hero js and css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import Navbar from './components/nav_bar';
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from './components/pages/home';
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        
+        <Routes>
+          <Route path='/' exact element = {<Home/>} />
+        </Routes>
       </Router>
     </>
   );
