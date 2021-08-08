6a9c4d780515a2d91287d64f123b1558f508b819
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-08 19:39:23 +0100

    add cards and card item

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../cards';
 import HeroSection from '../hero_section';
 
 function Home(){
     return (
         <>
         <HeroSection/>
+        <Cards />
         </>
     );
 }
