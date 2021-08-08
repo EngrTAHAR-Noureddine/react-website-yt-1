817d403dbfe91a26c7d289f5109f771be6487c48
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-08 20:37:01 +0100

    add footer

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import Cards from '../cards';
 import HeroSection from '../hero_section';
+import Footer from '../Footer';
 
 function Home(){
     return (
         <>
         <HeroSection/>
         <Cards />
+      <Footer />
         </>
     );
 }
