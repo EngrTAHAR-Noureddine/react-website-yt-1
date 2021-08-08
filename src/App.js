ac85a52a5da84ae61536c5ae16bdf092f52c42fb
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2021-08-08 20:36:49 +0100

    add services products sign-up pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import Navbar from './components/nav_bar';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './components/pages/home';
+import Services from './components/pages/services';
+import Products from './components/pages/products';
+import SignUp from './components/pages/sign_up';
+
 function App() {
   return (
     <>
@@ -10,6 +14,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' exact element = {<Home/>} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        
         </Routes>
       </Router>
     </>
